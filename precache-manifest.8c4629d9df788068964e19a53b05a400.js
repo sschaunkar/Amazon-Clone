4085410e4890624a7b295d37d8152c70df1c8bdb
sschaunkar <69849975+sschaunkar@users.noreply.github.com>
2020-10-14 10:32:27 +0530

    Updates

---
+++ precache-manifest.8c4629d9df788068964e19a53b05a400.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1954349d9afea431636247a995dcfb19",
+    "url": "/Amazon-Clone.git/index.html"
+  },
+  {
+    "revision": "aec1c9a5e25c72aa52d3",
+    "url": "/Amazon-Clone.git/static/css/main.bf93a613.chunk.css"
+  },
+  {
+    "revision": "ff1b1149a66172b4df21",
+    "url": "/Amazon-Clone.git/static/js/2.96dcf0fb.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/Amazon-Clone.git/static/js/2.96dcf0fb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aec1c9a5e25c72aa52d3",
+    "url": "/Amazon-Clone.git/static/js/main.2892a312.chunk.js"
+  },
+  {
+    "revision": "383a5bc5fdb68425aea3",
+    "url": "/Amazon-Clone.git/static/js/runtime-main.1016350e.js"
+  }
+]);
